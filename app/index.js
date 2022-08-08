1a381df9981d1d6527c0de8002a72d7b97bc6d09
vikamma <vikama@ukr.net>
2022-08-08 14:00:34 +0100

    Quiz

--- app/index.js
+++ app/index.js
@@ -1,1 +1,33 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+const customer = {
+  firstName: "Viktoriia",
+  lastName: "Matsiuk",
+  id: 33,
+  phoneNumber: "123456",
+  email: "vikama@ukr.net",
+};
+
+const address = {
+  street: "Locks Ride",
+  city: "London",
+  state: "London",
+  zipCode: "30456",
+};
+
+customer.address;
+console.log(`Welcome, ${customer.firstName} ${customer.lastName}`);
+
+const product = {
+  name: "Cat",
+  price: 200,
+  description: "Good cat",
+  quantity: 10,
+};
+
+const total = product.price * 7;
+console.log(`Total: ${total}`);
+
+if(total>100)
+ {console.log("True")};
+ else {console.log("False")};
+
+ product.quantity=3;
